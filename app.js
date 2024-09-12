4114f9e0b56e919b977b9e26f3422021bc3d78a3
Radster999 <radfas20@gmail.com>
2024-09-12 19:00:55 +0100

    blog

---
+++ app.js
@@ -0,0 +1,10 @@
+function resizeImage() {
+    const image = document.getElementById(images/lineup.png)
+    image.width = 300;
+    image.height = 200
+}
+function resizeImage() {
+    const image = document.getElementById(images/midweek-lineup.png)
+    image.width = 300;
+    image.height = 200
+}

409fa21d3a6d00c06bf73a788dae7a858ee843f2
Radster999 <radfas20@gmail.com>
2024-09-13 16:36:07 +0100

    blog

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function resizeImage() {
     const image = document.getElementById(images/lineup.png)
-    image.width = 20%;
+    image.width = 30%;
     image.height = 200
 }
 function resizeImage() {
     const image = document.getElementById(images/mid-lineup.png)
-    image.width = 20%;
+    image.width = 30%;
     image.height = 200
 }
